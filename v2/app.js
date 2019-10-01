21f10b63dbfdce0325db62aa7944cecb0940d893
Ketul Makwana <ketulmakwana09@gmail.com>
2019-10-01 18:19:55 +0530

    Added show template/route and description fiels to model

--- v2/app.js
+++ v2/app.js
@@ -10,19 +10,21 @@ app.set('view engine', 'ejs');
 
 const campgroundSchema = new mongoose.Schema({
     name:String,
-    image:String
+    image:String,
+    description: String
 });
 const Campground = mongoose.model('Campground', campgroundSchema);
 
-Campground.create(
-    {
-        name: 'Herkimer Diamond KOA',
-        image: 'https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
-    }, (err, newCampground) => {
-        if(err) console.log(err);
-        console.log(newCampground);
-    }
-)
+// Campground.create(
+//     {
+//         name: 'Herkimer Diamond KOA',
+//         image: 'https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+//         description: 'lLorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'
+//     }, (err, newCampground) => {
+//         if(err) console.log(err);
+//         console.log(newCampground);
+//     }
+// )
 
 const campgrounds = [
     {name: 'Prince Desert Camp', image: 'https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
@@ -40,18 +42,21 @@ app.get('/', (req, res) => {
     res.render('landing');
 })
 
+//INDEX - shows all campgrounds
 app.get('/campgrounds', (req, res) => {
     Campground.find({}, (err, allCampgrounds) => {
         if(err) console.log(err);
-        res.render('campgrounds', {campgrounds: allCampgrounds});
+        res.render('index', {campgrounds: allCampgrounds});
     })
 })
 
+//CREATE - add new campground to db
 app.post('/campgrounds', (req, res) => {
     //get data from form and add to campgrounds array
     const name = req.body.name;
     const image = req.body.image;
-    const newCampground = {name: name, image, image};
+    const desc = req.body.description;
+    const newCampground = {name: name, image, image, description: desc};
     //create a new campground and save it to a db
     Campground.create(newCampground, (err, newlyCreated) => {
         if(err) console.log(err);
@@ -61,10 +66,21 @@ app.post('/campgrounds', (req, res) => {
     
 })
 
+//NEW - shows a form to create new campground
 app.get('/campgrounds/new', (req, res) => {
     res.render('new');
 })
 
+//SHOW - shows more info about one campground
+app.get('/campgrounds/:id', (req, res) => {
+    //find a campground with provided ID
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if(err) console.log(err);
+        //render show template with that campground
+        res.render('show', {campground: foundCampground});
+    })
+    
+})
 app.listen(8000, () => {
     console.log('Server Started!');
 })
